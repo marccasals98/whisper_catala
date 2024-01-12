c73f0376437c970bee78b4f065779fc9e0778208
marccasals98 <marc.casals8@gmail.com>
2024-01-12 09:13:31 +0000

    Added files to run SLURM

---
+++ src/shs_container/main.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --output=src/logs/slurm-%j.out  # Standard output and error log
+#SBATCH --error=src/errors/slurm-%j.err
+#SBATCH --partition=veu
+#SBATCH --job-name=whisper_finetune    # Job name
+#SBATCH --cpus-per-task=4                  # Run on 4 CPU
+#SBATCH --mem=128G                    # Job memory request
+#SBATCH --gres=gpu:4
+
+python3 src/main.py

ebea511d3bf00e18c54b55339f333483e08f84e4
marccasals98 <marc.casals8@gmail.com>
2024-01-31 12:49:37 +0100

    Changed batch size to 1 and compatibility of libraries

--- src/config.py
+++ src/config.py
@@ -13,7 +13,7 @@ DATASET =   {
 
 training_args = Seq2SeqTrainingArguments(
     output_dir="./whisper-small-hi",  # change to a repo name of your choice
-    per_device_train_batch_size=16,
+    per_device_train_batch_size=1,
     gradient_accumulation_steps=1,  # increase by 2x for every 2x decrease in batch size
     learning_rate=1e-5,
     warmup_steps=500,
@@ -21,7 +21,7 @@ training_args = Seq2SeqTrainingArguments(
     gradient_checkpointing=True,
     fp16=True,
     evaluation_strategy="steps",
-    per_device_eval_batch_size=8,
+    per_device_eval_batch_size=1,
     predict_with_generate=True,
     generation_max_length=225,
     save_steps=1000,
--- src/shs_container/main.sh
+++ src/shs_container/main.sh
@@ -4,7 +4,7 @@
 #SBATCH --partition=veu
 #SBATCH --job-name=whisper_finetune    # Job name
 #SBATCH --cpus-per-task=4                  # Run on 4 CPU
-#SBATCH --mem=128G                    # Job memory request
-#SBATCH --gres=gpu:4
+#SBATCH --mem=32G                    # Job memory request
+#SBATCH --gres=gpu:5
 
 python3 src/main.py

8468a6c0c24dcab60b68dafed7ee4b9d7f1af531
marccasals98 <marc.casals8@gmail.com>
2024-02-05 14:41:47 +0100

    changed the requirements

--- create_venv.sh
+++ create_venv.sh
@@ -1,3 +1,24 @@
+# PYTHON VIRTUAL ENVIRONMENT
+
+#!/bin/bash
+
+venv_name="whisper_venv"
+
+# Check if the virtual environment exists
+if [ -d "$venv_name" ]; then
+    echo "Virtual environment '$venv_name' already exists."
+else
+    # Create the virtual environment
+    python3 -m venv "$venv_name"
+
+    if [ $? -eq 0 ]; then
+        echo "Virtual environment '$venv_name' created successfully."
+    else
+        echo "Error creating virtual environment '$venv_name'."
+    fi
+fi
+source whisper_venv/bin/activate
+
 pip install -r requirements.txt
-pip install accelerate -U
-pip install transformers[torch]
+# pip install accelerate -U
+# pip install transformers[torch]
--- install.sh
+++
@@ -1,3 +0,0 @@
-pip install -r requirements.txt
-pip install accelerate -U
-pip install transformers[torch]

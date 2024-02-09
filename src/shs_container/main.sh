87983d4bde2785a55d51c45c2b174d2196998aa9
marccasals98 <marc.casals8@gmail.com>
2024-02-09 15:34:48 +0100

    modified to run on the cluster

--- src/shs_container/main.sh
+++ src/shs_container/main.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+#SBATCH -D .
 #SBATCH --output=src/logs/slurm-%j.out  # Standard output and error log
 #SBATCH --error=src/errors/slurm-%j.err
-#SBATCH --partition=veu
 #SBATCH --job-name=whisper_finetune    # Job name
-#SBATCH --cpus-per-task=4                  # Run on 4 CPU
+#SBATCH --cpus-per-task=1                  # Run on 4 CPU
 #SBATCH --mem=32G                    # Job memory request
-#SBATCH --gres=gpu:5
-
+#SBATCH --gres=gpu:1
+export HF_DATASETS_OFFLINE=1 
+export TRANSFORMERS_OFFLINE=1
 module load singularity/3.6.4
-singularity exec /gpfs/projects/bsc88/singularity-images/whisper_catala python src/main.py
+singularity exec /gpfs/projects/bsc88/singularity-images/whisper-catala python src/main.py
 
